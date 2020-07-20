c0cd840f2313220d23c86327baf743e0020052ac
lyf2000 <lyf2000@mail.ru>
2020-07-20 15:27:14 +0300

    add dates,formats to srlzr

--- backend/events/serializers.py
+++ backend/events/serializers.py
@@ -3,7 +3,19 @@ from .models import Event
 
 class EventSerializer(serializers.ModelSerializer):
     date = serializers.DateTimeField(format='%b %d, %H:%M')
+    updated = serializers.DateTimeField(format='%b %d, %H:%M')
+    created = serializers.DateTimeField(format='%b %d')
+
+    def __init__(self, *args, **kwargs):
+
+        # TODO determine either list or eiter detail
+        super(serializers.ModelSerializer, self).__init__(*args, **kwargs)
+        # a = self.context['request']
+        # self.fields.pop('date')
+        
+
+        
 
     class Meta:
         model = Event
-        fields = ('id', 'title', 'text', 'date', )
+        fields = ('id', 'title', 'text', 'date', 'created', 'updated', )
