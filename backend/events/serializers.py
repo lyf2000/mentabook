ec967b19ba2d12865518f80e897a61464ba64821
lyf2000 <lyf2000@mail.ru>
2020-07-20 14:58:49 +0300

    add date,format

--- backend/events/serializers.py
+++ backend/events/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 from .models import Event
 
 class EventSerializer(serializers.ModelSerializer):
+    date = serializers.DateTimeField(format='%b %d, %H:%M')
 
     class Meta:
         model = Event
-        fields = ('id', 'title', 'text', )
+        fields = ('id', 'title', 'text', 'date', )
