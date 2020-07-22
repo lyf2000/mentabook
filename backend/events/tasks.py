d2a4454992944fa6b5e81d27e81c7d3f6e50fb55
lyf2000 <lyf2000@mail.ru>
2020-07-22 09:21:11 +0300

    add simple reminder

--- backend/events/tasks.py
+++ backend/events/tasks.py
@@ -4,8 +4,6 @@ from celery import shared_task
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.timezone import localtime
-
-
 from .models import Event
 
 
