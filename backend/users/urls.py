29ecbd301e0670396cd08dc6f98430b587752ad5
lyf2000 <lyf2000@mail.ru>
2020-07-22 15:37:36 +0300

    signup

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,6 +3,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from .views import UserCreateAPIView
 
 app_nane = 'users'
 
@@ -11,5 +12,5 @@ app_nane = 'users'
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
+    path('signup/', UserCreateAPIView.as_view(), name='signup'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import CreateAPIView
+from .serializers import UserCreateSerializer
 
-# Create your views here.
+
+class UserCreateAPIView(CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserCreateSerializer
