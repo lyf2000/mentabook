0622e1cd5442bb2510b99eab13b5f64bdaa9d05d
lyf2000 <lyf2000@mail.ru>
2020-07-31 08:28:50 +0300

    works!

--- Dockerfile
+++ Dockerfile
@@ -15,28 +15,21 @@ COPY Pipfile Pipfile.lock ./
 
 
 
-ENV DEBUG True
+ENV DEBUG False
 ENV CELERY_BROKER_URL redis://redis:6379
 ENV CELERY_RESULT_BACKEND redis://redis:6379
 ENV ADMIN_GOOGLE_EMAIL nda030600@gmail.com
 ENV ADMIN_GOOGLE_PASSWORD LFVBH0110
 ENV VUE_APP_BACKEND_HOST http://localhost:1234
-ENV FRONTEND_URL http://localhost:8080
+ENV FRONTEND_URL http://localhost:1234
 ENV DB_NAME postgres
 ENV DB_USER postgres
 ENV DB_PASSWORD postgres
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-# install psycopg2 dependencies
-RUN apt-get update && \
-    apt-get install -y --no-install-recommends build-essential
-
-# install dependencies
-RUN pip install --upgrade pip
-
 RUN pip install pipenv
 RUN pipenv install --system 
-RUN python manage.py migrate
+# RUN python manage.py migrate
 
 CMD python manage.py runserver  0.0.0.0:8000
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -94,14 +94,23 @@ WSGI_APPLICATION = 'backend.core.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.getenv('DB_NAME'),
+    #     'USER': os.getenv('DB_USER'),
+    #     'PASSWORD': os.getenv('DB_PASSWORD'),
+    #     'HOST' : os.getenv('DB_HOST', 'db'),
+    #     'PORT': 5432,
+    # }
+
+
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST' : os.getenv('DB_HOST', 'db'),
-        'PORT': 5432,
-    }
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db', # set in docker-compose.yml
+        'PORT': 5432 # default postgres port
+    },
 
 
     # 'default': {
@@ -188,8 +197,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 # CORS
-if DEBUG:
-    CORS_ORIGIN_WHITELIST = [
-        os.getenv('FRONTEND_URL'),
-    ]
-# CORS_ORIGIN_ALLOW_ALL = True
+# if DEBUG:
+# CORS_ORIGIN_WHITELIST = [
+#     os.getenv('FRONTEND_URL'),
+# ]
+CORS_ORIGIN_ALLOW_ALL = True
